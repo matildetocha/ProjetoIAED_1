f4c523d10150f85f23bd90966aa4aca3c82644fa
matildetocha <matilde.tocha@tecnico.ulisboa.pt>
2021-04-08 22:44:58 +0100

    AddUser funcional + comando 'u' ; AddTask experimental ; variáveis globais (user_number & task_number); estrutura main alterada; comentários e defines.
    Co-authored-by: MisterrHappy <MisterrHappy@users.noreply.github.com>

--- p1.c
+++ p1.c
@@ -1,7 +1,7 @@
 /*
- * File:  structures.c
+ * File:  p1.c
  * Author:  Matilde Tocha 99108
- * Description: Programa com as três estruturas relevantes para o projeto. Tarefa, atividade e utilizador.
+ * Description: Projecto 1 - IAED 2020/21
 */
 
 #include <stdio.h>
@@ -9,23 +9,24 @@
 #include <ctype.h>
 #include <string.h>
 
-/* Número máximo de caracteres para descrever um utilizador ou uma atividade */
+/* Número máximo de caracteres para descrever um utilizador ou uma atividade. */
 #define SZ_DES_UA 21
-/* Número máximo de caracteres para descrever uma tarefa */
+/* Número máximo de caracteres para descrever uma tarefa. */
 #define SZ_DES_T 51
-/* Número máximo de utilizadores que o sistema suporta */
-#define SZ_USERS 51
-/* Número máximo de tarefas que o sistema suporta */
+/* Número máximo de utilizadores que o sistema suporta. */
+#define SZ_USERS 50
+/* Número máximo de tarefas que o sistema suporta. */
 #define SZ_TASKS 10000
+/* Número máximo de atividades que o sistema suporta. */
+#define SZ_ACT 10
 /* qual é o número máximo de instruções que o sitema aceita?? */
 #define MAX 100
 
+/* Structs */
 typedef struct
 {
     char des_user[SZ_DES_UA];
-    int user_number;
 } User;
-
 User system_users[SZ_USERS];
 
 typedef struct
@@ -33,8 +34,7 @@ typedef struct
     char des_act[SZ_DES_UA];
 
 } Activity;
-
-Activity activities[10];
+Activity activities[SZ_ACT];
 
 typedef struct
 {
@@ -45,9 +45,35 @@ typedef struct
     Activity task_act;
     int starting_time;
 } Task;
-
 Task tasks[SZ_TASKS];
 
+/* The number of users stored. */
+int user_number = 0;
+/* The number of tasks stored. */
+int task_number = 0;
+
+/* Adds a new given task to the global vector of tasks. */
+void AddTask(Task t)
+{
+    int i = 0, cmp = 1;
+
+    for (i = 0; i < SZ_TASKS; i++)
+    {
+        if (strcmp(t.des_task, tasks[i].des_task) == 0)
+        {
+            printf("duplicate description\n");
+            return;
+        }
+    }
+    if (strlen(tasks->des_task) == SZ_TASKS)
+    {
+        printf("too many tasks\n");
+        return;
+    }
+    else strcpy(tasks[1].des_task, t.des_task);
+}
+
+/* Adds a new given user to the global vector of system_users. */
 void AddUser(User v)
 {
     int i = 0;
@@ -55,59 +81,81 @@ void AddUser(User v)
     for (i = 0; i < SZ_USERS; i++)
     {
         if (strcmp(v.des_user, system_users[i].des_user) == 0)
+        {
             printf("user already exists\n");
+            user_number--;
+            return;
+        }
     }
-    printf("%ld\n", strlen(system_users->des_user));
     if (strlen(system_users->des_user) == SZ_USERS)
+    {
         printf("too many users\n");
-    else
-        strcpy(system_users[v.user_number].des_user, v.des_user);
+        user_number--;
+        return;
+    }
+    else strcpy(system_users[user_number].des_user, v.des_user);
+}
+
+void BreakStr(char str[], const char delim[], char *v[])
+{
+    char *token;
+    int i = 0;
+
+    /* Gets the first token. */
+    token = strtok(str, " \n");
+
+    /* Walks through other tokens. */
+    while (token != NULL)
+    {
+        v[i++] = token;
+        token = strtok(NULL, delim);
+    }
+    v[i] = NULL;
 }
 
 int main()
 {
-    char str[MAX];
-    int c;
+    char str[MAX], *v[MAX];
     int i = 0;
     User u;
 
     strcpy(activities[0].des_act, "TO DO");
     strcpy(activities[1].des_act, "IN PROGRESS");
     strcpy(activities[2].des_act, "DONE");
-    strcpy(system_users[0].des_user, "PedroMariaSilva");
-    strcpy(system_users[1].des_user, "abc");
 
-    while ((c = getchar()) != 'q')
+    for (;;)
     {
-        getchar(); /* Retira o carácter em branco entre o comando e as devidas instruções */
         fgets(str, MAX, stdin);
-
-        switch (c)
+        switch (str[0])
         {
-        case 't':
-            break;
-        case 'l':
-            break;
-        case 'n':
-            break;
-        case 'u':
-            if (1)
-            {
-                strcpy(u.des_user, strtok(str, " \n"));
-                u.user_number++;
-                AddUser(u);
-            }
-            for (i = 0; i < SZ_USERS; i++)
-                printf("%s\n", system_users[i].des_user);
-            break;
-        case 'm':
-            break;
-        case 'd':
-            break;
-        case 'a':
-            break;
+            case 't':
+                break;
+            case 'l':
+                break;
+            case 'n':
+                break;
+            case 'u':
+                BreakStr(str, "\n", v);
+                if (v[1] == NULL)
+                {
+                    for (i = 0; i < user_number; i++)
+                        printf("%s\n", system_users[i].des_user);
+                }
+                else
+                {
+                     strcpy(u.des_user, v[1]);
+                    AddUser(u);
+                    user_number++;                   
+                }
+                break;
+            case 'm':
+                break;
+            case 'd':
+                break;
+            case 'a':
+                break;
+            case 'q':
+                exit(EXIT_SUCCESS);
         }
     }
-
-    exit(EXIT_SUCCESS);
 }
